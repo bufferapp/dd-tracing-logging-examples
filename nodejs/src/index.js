be6586a70bf6c17840e4e327b9ffbe5ddcc0f05d
Steven Cheng <stevenc81@gmail.com>
2019-05-08 14:56:09 -0700

    first commit

---
+++ nodejs/src/index.js
@@ -0,0 +1,35 @@
+require('dd-trace').init({
+	hostname: process.env.DD_AGENT_HOST,
+	port: 8126,
+	env: 'development',
+	logInjection: true,
+});
+
+const { createLogger, format, transports } = require('winston');
+const addAppNameFormat = format(info => {
+  info.ddtags = {'logging-mvp': 'dd-tracing-logging-examples-nodejs'};
+  return info;
+});
+const logger = createLogger({
+  level: 'info',
+  exitOnError: false,
+  format: format.combine(
+  	addAppNameFormat(),
+  	format.json(),
+  	),
+  transports: [
+    new transports.Console(),
+  ],
+});
+
+const express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+	logger.log('info', 'A simple log that works with Datadog APM tracing and logging!');
+	res.send('Hello world, this will demo Datadog tracing and logging!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
